34ba7956efa8717afdf5d20dcc74d0f9981e0570
Naveneetha Vasudevan <kvasudevan@smartcommunications.com>
2024-03-28 10:57:45 +0000

    Add terraform file to create the necessary AWS resources for packer build

---
+++ terraform/input.tf
@@ -0,0 +1,7 @@
+variable "build_vpc_id" {
+  type = string
+}
+
+variable "source_ip_cidr" {
+  type = string
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+build_vpc_id   = "vpc-xxx"
+source_ip_cidr = "10.10.1.0/8" # private subnet in aws
